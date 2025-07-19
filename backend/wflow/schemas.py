c92366bddde33881b1a8196a345ddd27e2063706
iDhanush <idhanushm@gmail.com>
2025-07-20 00:53:24 +0530

    tools schmas updated

--- backend/wflow/schemas.py
+++ backend/wflow/schemas.py
@@ -33,55 +33,61 @@ class WFlow(BaseModel):
     nodes: list[Node]
     connections: list[Conn]
 
+
 #
-# sample_workflow = WFlow(
-#     wflow_name='str',
-#     user_id='',
-#     wflow_id='wfl_OLASjlajnfJ',
-#     nodes=[
-#         Node(
-#             node_id="whatsapp_trigger_1",
-#             purpose="",
-#             type="trigger",
-#             node_class="WhatsappTrigger",
-#             position={"x": 100, "y": 100},
-#             tools=[],
-#             creds=[],
-#         ),
-#         Node(
-#             node_id="image_agent_1",
-#             type="agent",
-#             node_class="ImageAgent",
-#             purpose="Generate Image for the prompt given",
-#             position={"x": 300, "y": 100},
-#             tools=[{"generate_image": True}],
-#             creds=[],
-#         ),
-#         Node(
-#             node_id="telegram_agent_1",
-#             type="agent",
-#             node_class="TelegramAgent",
-#             purpose="if the image generated is of a cat then send it to user 868213406 else do nothing",
-#             position={"x": 500, "y": 100},
-#             tools=[{"send_message": True}, {"send_image": True}],
-#             creds=[],
-#         ),
-#         Node(
-#             node_id="whatsapp_agent_1",
-#             type="agent",
-#             node_class="WhatsappAgent",
-#             purpose="if the image generated is of a dog then send it to the number 9995539972 else do nothing",
-#             position={"x": 500, "y": 100},
-#             tools=[{"send_message": True}, {"send_image": True}], creds=[],
-#         )
-#     ],
-#     connections=[
-#         Conn(conn_id='whatsapp_trigger_1_to_image_agent_1', from_node="whatsapp_trigger_1", to_node="image_agent_1"),
-#         Conn(conn_id='image_agent_1_to_telegram_agent_1', from_node="image_agent_1", to_node="telegram_agent_1"),
-#         Conn(conn_id='image_agent_1_to_whatsapp_agent_1', from_node="image_agent_1", to_node="whatsapp_agent_1"),
-#     ]
-# )
-# print(sample_workflow.model_dump())
+sample_workflow = WFlow(
+    wflow_name='str',
+    user_id='',
+    wflow_id='wfl_OLASjlajnfJ',
+    nodes=[
+        Node(
+            node_id="whatsapp_trigger_1",
+            purpose="",
+            type="trigger",
+            node_class="WhatsappTrigger",
+            position={"x": 100, "y": 100},
+            tools=[],
+            creds=[],
+        ),
+        Node(
+            node_id="image_agent_1",
+            type="agent",
+            node_class="ImageAgent",
+            purpose="Generate Image for the prompt given",
+            position={"x": 300, "y": 100},
+            tools=[Tool(tool_func="generate_image", label="asdasd",
+                        description="dedsa", active=True)],
+            creds=[],
+        ),
+        Node(
+            node_id="telegram_agent_1",
+            type="agent",
+            node_class="TelegramAgent",
+            purpose="if the image generated is of a cat then send it to user 868213406 else do nothing",
+            position={"x": 500, "y": 100},
+            tools=[Tool(tool_func="send_message", label="asdasd",
+                        description="dedsa", active=True), Tool(tool_func="send_image", label="asdasd",
+                                                                description="dedsa", active=True)],
+            creds=[],
+        ),
+        Node(
+            node_id="whatsapp_agent_1",
+            type="agent",
+            node_class="WhatsappAgent",
+            purpose="if the image generated is of a dog then send it to the number 9995539972 else do nothing",
+            position={"x": 500, "y": 100},
+            tools=[Tool(tool_func="send_message", label="asdasd",
+                        description="dedsa", active=True), Tool(tool_func="send_image", label="asdasd",
+                                                                description="dedsa", active=True)], creds=[],
+        )
+    ],
+    connections=[
+        Conn(conn_id='whatsapp_trigger_1_to_image_agent_1', from_node="whatsapp_trigger_1", to_node="image_agent_1"),
+        Conn(conn_id='image_agent_1_to_telegram_agent_1', from_node="image_agent_1", to_node="telegram_agent_1"),
+        Conn(conn_id='image_agent_1_to_whatsapp_agent_1', from_node="image_agent_1", to_node="whatsapp_agent_1"),
+    ]
+)
+print(sample_workflow.model_dump())
 
 
 class WFlowPayload(BaseModel):
