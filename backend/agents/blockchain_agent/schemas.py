e34536de0a04560fee7d66d0314b9f8ae04c62dc
iDhanush <idhanushm@gmail.com>
2025-07-19 22:50:16 +0530

    schemas for contract saving

--- backend/agents/blockchain_agent/schemas.py
+++ backend/agents/blockchain_agent/schemas.py
@@ -8,12 +8,23 @@ class AgentState(TypedDict):
     response: Optional[str | dict]
     user_id: str
 
+
 class TransferInput(BaseModel):
     to: str = Field(..., description="The address to transfer to")
     amount: float = Field(..., description="The amount to transfer in eth")
 
+
 class NftInput(BaseModel):
     url: str = Field(..., description="The url of the nft")
 
+
 class NoInput(BaseModel):
     pass
+
+
+class ContractsData(BaseModel):
+    user_id: str
+    wallet: str
+    network: str
+    fund_me: str
+    simplecol: str
