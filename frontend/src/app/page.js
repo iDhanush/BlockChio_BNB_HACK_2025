a6c0a2d2c75898bc4bd6a7f0c9bdae3115bd8734
Abhinav K E <109943270+Abhinav-K-E@users.noreply.github.com>
2025-07-19 14:48:28 +0530

    landing page styles

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,15 +3,17 @@ import TopNav from "@/components/TopNav/TopNav";
 import "./app.scss";
 const page = () => {
   return (
-    <div className="hero">
-      <div className="hero-linear"></div>
-      <TopNav />
-      <div className="main-sec">
-        <div className="main-txt">
-          Create Your Own<br></br>Decentralized <span>AI Agents</span>
-        </div>
-        <div className="hero-sub-txt">
-          Start building your AI agents with full control and transparency.
+    <div className="home">
+      <div className="hero">
+        <TopNav />
+        <div className="hero-sec">
+          <div className="no-projects">
+            <div className="sec-head">Your WorkFlow</div>
+            <div className="sec-txt">
+              Start building powerful AI workflows by<br></br> adding your first
+              project.
+            </div>
+          </div>
         </div>
       </div>
     </div>
