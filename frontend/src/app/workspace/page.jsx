1e3d0cbeb83b02914a6a1b14781b192b46bc701c
Abhinav K E <109943270+Abhinav-K-E@users.noreply.github.com>
2025-07-20 02:07:51 +0530

    workflows added

--- frontend/src/app/workspace/page.jsx
+++ frontend/src/app/workspace/page.jsx
@@ -62,24 +62,26 @@ const Page = () => {
             Start building powerful AI workflows by
             <br /> adding your first project.
           </div>
-          <div className="add-project-btn" onClick={handleNewProjectClick}>
-            <div className="add-icon">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width={27}
-                height={26}
-                fill="none"
-              >
-                <path
-                  stroke="#F3F4F5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={3}
-                  d="M13.5 1.5v23M2 13h23"
-                />
-              </svg>
+          <div className="workflow-cards">
+            <div className="add-project-btn" onClick={handleNewProjectClick}>
+              <div className="add-icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={27}
+                  height={26}
+                  fill="none"
+                >
+                  <path
+                    stroke="#F3F4F5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={3}
+                    d="M13.5 1.5v23M2 13h23"
+                  />
+                </svg>
+              </div>
+              <div className="btn-txt">New Project</div>
             </div>
-            <div className="btn-txt">New Project</div>
           </div>
         </div>
       </div>
