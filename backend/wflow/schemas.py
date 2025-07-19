209e2ee45c1feeabd0d66cc2d42543333d7186d9
iDhanush <idhanushm@gmail.com>
2025-07-19 13:13:19 +0530

    sample workflow for ai image gen and telegram

--- backend/wflow/schemas.py
+++ backend/wflow/schemas.py
@@ -24,3 +24,49 @@ class WorkFlow(BaseModel):
     conns: list[Conn]
 
 
+sample_workflow = WorkFlow(
+    user_id='',
+    id='wfl_OLASjlajnfJ',
+    nodes=[
+        Node(
+            id="whatsapp_trigger_1",
+            purpose="",
+            type="trigger",
+            node_class="WhatsappTrigger",
+            pos={"x": 100, "y": 100},
+            tools=[],
+            cred={},
+        ),
+        Node(
+            id="image_agent_1",
+            type="agent",
+            node_class="ImageAgent",
+            purpose="Generate Image for the prompt given",
+            pos={"x": 300, "y": 100},
+            tools=[{"generate_image": True}],
+            cred={},
+        ),
+        Node(
+            id="telegram_agent_1",
+            type="agent",
+            node_class="TelegramAgent",
+            purpose="if the image generated is of a cat then send it to user 868213406 else do nothing",
+            pos={"x": 500, "y": 100},
+            tools=[{"send_message": True}, {"send_image": True}],
+            cred={},
+        ),
+        Node(
+            id="whatsapp_agent_1",
+            type="agent",
+            node_class="WhatsappAgent",
+            purpose="if the image generated is of a dog then send it to the number 9995539972 else do nothing",
+            pos={"x": 500, "y": 100},
+            tools=[{"send_message": True}, {"send_image": True}], cred={},
+        )
+    ],
+    conns=[Conn(id='whatsapp_trigger_1_to_image_agent_1', from_node="whatsapp_trigger_1", to_node="image_agent_1"),
+           Conn(id='image_agent_1_to_telegram_agent_1', from_node="image_agent_1", to_node="telegram_agent_1"),
+           Conn(id='image_agent_1_to_whatsapp_agent_1', from_node="image_agent_1", to_node="whatsapp_agent_1"),
+           ]
+)
+print(sample_workflow.model_dump())
