cf953b498d84172347589863147306b73a89f32a
iDhanush <idhanushm@gmail.com>
2025-07-20 02:32:45 +0530

    execute workflow

--- backend/wflow/api.py
+++ backend/wflow/api.py
@@ -1,14 +1,12 @@
 from random import sample
 
 from fastapi import APIRouter
-
 from auth.auth import get_user
 from auth.schemas import User
 from globar_vars import Var
 from wflow.schemas import WFlow, WFlowPayload, sample_workflow
 from utils.tokenizer import invoke_uid
 from fastapi import Depends
-
 from wflow.wflow import WorkflowExecutor
 
 wflow_router = APIRouter(prefix='/wflow')
@@ -37,9 +35,14 @@ async def update_wflow(wflow_id: str, wflow_payload: WFlowPayload, _user: User =
     await Var.db.set_wflow(wflow_id, wflow_payload)
 
 
-@wflow_router.get('/{wflow_id}/execute')
+@wflow_router.post('/{wflow_id}/execute')
 async def execute_wflow(wflow_id: str, wflow_payload: WFlowPayload, _user: User = Depends(get_user)):
     await Var.db.set_wflow(wflow_id, wflow_payload)
     wflow_data = await Var.db.get_wflow(wflow_id)
     executor = WorkflowExecutor(workflow=wflow_data)
     await executor.execute("create a cute cat")
+
+@wflow_router.get('/workflows')
+async def list_workflows(user:User= Depends(get_user)):
+    wflow_list = await Var.db.get_wflows(user.user_id)
+    return {'wflow_list': wflow_list}
