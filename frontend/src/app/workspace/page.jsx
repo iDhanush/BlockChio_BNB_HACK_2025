0bce5902bc34f145ea3e4561d96e4cc88cf59b5c
Abhinav K E <109943270+Abhinav-K-E@users.noreply.github.com>
2025-07-19 12:40:09 +0530

    workspace added

--- frontend/src/app/workspace/page.jsx
+++ frontend/src/app/workspace/page.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+"use client";
+import "./workspace.scss";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="workspace-sec">
+      <div className="no-projects">
+        <div className="sec-head">Your WorkFlow</div>
+        <div className="sec-txt">
+          Start building powerful AI workflows by<br></br> adding your first
+          project.
+        </div>
+        <div className="add-project-btn">
+          <div className="add-icon">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={27}
+              height={26}
+              fill="none"
+            >
+              <path
+                stroke="#F3F4F5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={3}
+                d="M13.5 1.5v23M2 13h23"
+              />
+            </svg>
+          </div>
+          <div className="btn-txt">New Project</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
